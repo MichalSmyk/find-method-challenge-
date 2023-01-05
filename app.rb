b061c7f61413a0857a041db3fe7f4948e10176d6
MichalSmyk <michal.smyk39@gmail.com>
2023-01-05 16:07:48 +0000

    debugged

--- app.rb
+++ app.rb
@@ -11,9 +11,11 @@ recipe_repository = RecipeRepository.new
 
 
 # Print out each recipe from the result set .
-#
+# recipe_repository.all.each do |recipe|
+#   p "#{recipe.id} - #{recipe.recipe_name} - #{recipe.cooking_time} - #{recipe.rating} "
+# end
 
 #print a recipe based on given id 
 
-recipe = recipe_repository.find(2)
+recipe = recipe_repository.find(1)
 puts recipe.recipe_name
--- spec/seeds_test.sql
+++ spec/seeds_test.sql
@@ -3,5 +3,5 @@ TRUNCATE TABLE recipes RESTART IDENTITY; -- replace with your own table name.
 -- Below this line there should only be `INSERT` statements.
 -- Replace these statements with your own seed data.
 
-INSERT INTO recipes (recipe_name, cooking_time, rating) VALUES ('pasta', '22', '4');
+INSERT INTO recipes (recipe_name, cooking_time, rating) VALUES ('pastalalal', '22', '4');
 INSERT INTO recipes (recipe_name, cooking_time, rating) VALUES ('hot dog', '2', '1');
